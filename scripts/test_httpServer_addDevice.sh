db2857c784fda8dcef7a530284cdf24181ea1a0c
chenghua.wang <2923277184@qq.com>
2022-11-20 21:25:32 +0800

    fix some curl bugs

---
+++ scripts/test_httpServer_addDevice.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -H "Content-Type: application/json" -X POST -d "{\"host\": \"27.208.84.114\",\"port\": \"3306\",\"usrName\": \"django_user\", \"password\": \"django_user\", \"databaseName\": \"testdb\", \"deviceType\": \"MySQL\"}" "http://localhost:8080/add_device"
