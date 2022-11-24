418290b513bd724933be9eab4de16935ed9cca81
chenghua.wang <2923277184@qq.com>
2022-11-24 11:04:07 +0800

    Translate Lua to Json

---
+++ scripts/utils/TransLua2Json.py
@@ -0,0 +1,16 @@
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--src')
+parser.add_argument('--dst')
+parser.add_argument('--idx')
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    data = {'id': args.idx}
+    with open(args.src, 'r') as f:
+        strs = f.read()
+    data.update({'script': strs})
+    f = open(args.dst, 'w')
+    json.dump(data, f)
